eafb74fc7355deb28495b55c563019d80bdf39e7
jcreollo00 <joseph.creollo@gmail.com>
2025-09-21 20:22:43 -1000

    Updated a lot

---
+++ sumofnumbers.ts
@@ -0,0 +1,38 @@
+// Sum using for-loop
+function sumFor(numbers: number[]): number {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
+
+// Sum using while-loop
+function sumWhile(numbers: number[]): number {
+  let sum = 0;
+  let i = 0;
+  while (i < numbers.length) {
+    sum += numbers[i];
+    i++;
+  }
+  return sum;
+}
+
+// Sum using recursion
+function sumRecursion(numbers: number[]): number {
+  if (numbers.length === 0) {
+    return 0;
+  }
+  return numbers[0] + sumRecursion(numbers.slice(1));
+}
+
+// Sum using functional programming
+function sumTheFunctionalWay(numbers: number[]): number {
+  return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+
+// Test the functions
+console.log(sumFor([1, 2, 3, 4])); // Output: 10
+console.log(sumWhile([1, 2, 3, 4])); // Output: 10
+console.log(sumRecursion([1, 2, 3, 4])); // Output: 10
+console.log(sumTheFunctionalWay([1, 2, 3, 4])); // Output: 10
